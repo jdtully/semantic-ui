bde1bce308d3a8c820468e8f9124b32759946c03
Jeff <jefftully@gmail.com>
2019-09-09 15:02:06 -0400

    initial commit

---
+++ src/Login.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment
+} from "semantic-ui-react";
+
+export default () => (
+  <Grid centered columns={2}>
+    <Grid.Column>
+      <Header as="h2" textAlign="center">
+        login
+      </Header>
+      <Segment>
+        <Form size="large">
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="email
+        address"
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Password"
+            type="password"
+          />
+          <Button color="blue" fluid size="large">
+            Login
+          </Button>
+        </Form>
+      </Segment>
+      <Message>
+        {" "}
+        Not Registered Yet? <a href="#">Sign Up</a>
+      </Message>
+    </Grid.Column>
+  </Grid>
+);
